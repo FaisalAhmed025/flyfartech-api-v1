58b45859b9534623dd616ee9cca6d4e2e71b2574
mdfaisalahmed025 <122660563+FaisalAhmed025@users.noreply.github.com>
2023-06-04 14:29:11 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(port);
-  app.enableCors
+  app.enableCors()
   
 }
 bootstrap();

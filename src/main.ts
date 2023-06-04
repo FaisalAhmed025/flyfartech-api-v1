15db46d022576da4e764604880818e744ede2641
mdfaisalahmed025 <122660563+FaisalAhmed025@users.noreply.github.com>
2023-06-04 15:13:41 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await  app.enableCors();
+  await app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('FlyFar tech')
   .setDescription('Flyfar tech Description')

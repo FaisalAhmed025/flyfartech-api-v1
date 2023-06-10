385f2d4c4c8b5394c9cd97b1d423557c00bceb65
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2023-06-10 10:21:00 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const port = process.env.PORT || 5000
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HelmetMiddleware } from '@nest-middlewares/helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +14,6 @@ async function bootstrap() {
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.use(HelmetMiddleware)
   await app.listen(port);
 }
 bootstrap();

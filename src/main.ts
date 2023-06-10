2cc45400b0d759a05472bc1e7717b80d930efafb
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-06-10 10:32:35 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.enablecors();
+  app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('FlyFar Tech')
   .setDescription('FlyFar Tech')
